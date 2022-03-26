f682063e7051ea0dc15d2bfb6754143042691193
Emmanuel Etti <emmanueletti@gmail.com>
2022-03-25 22:11:13 -0600

    adds ability to delete an article

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   # Need to have the symbol be of a resource already created in the db schema and
   # managed via a model file in the model folder.
   # Can select just the routes needed by adding only: [:ROUTE_CONTROLLER_ACTION_NAME]
-  # e.g resources :articles, only: [:show]
+  # e.g resources :articles, only: [:show, :index, :create]
 
   # to see all current routes in the app run "rails routes --expanded" in the terminal.
-  resources :articles, only: %i[show index new create edit update]
+  resources :articles
 end
