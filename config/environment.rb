741ee6deb5b4f30e6bc886b1ba8bd2f4f0f4cb6f
Emmanuel Etti <emmanueletti@gmail.com>
2022-03-30 20:07:21 -0700

    adds styling to success and error messages

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# code to remove RAILS added div.field_with_errors
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
+end
